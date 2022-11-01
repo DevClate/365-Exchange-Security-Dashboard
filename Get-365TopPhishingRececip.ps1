9da5a611a8d05d75fb04d67ebc554e75645c14b4
DevClate <107187714+DevClate@users.noreply.github.com>
2022-11-01 07:05:08 -0400

    Created Top Phishing Function

---
+++ Get-365TopPhishingRececip.ps1
@@ -0,0 +1,30 @@
+function Get-365TopPhishingRecip {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [string]
+        $UserPrincipalName,
+        [Parameter(Mandatory,HelpMessage='Enter start time, ex 04/01/2022')]
+        [datetime]
+        $StartDate,
+        [Parameter(Mandatory,HelpMessage='Enter end date, ex 04/30/2022')]
+        [datetime]
+        $EndDate,
+        [Parameter()]
+        [string]
+        $Path = 'C:\Scripts\TopPishingReport.xlsx'
+    )
+    
+    begin {
+        Connect-ExchangeOnline -UserPrincipalName $UserPrincipalName
+    }
+    
+    process {
+        #Top Phishing Recipients
+        Get-MailTrafficSummaryReport -Category TopphishRecipient –StartDate $startdate -EndDate $enddate | Select-Object C1,C2
+    }
+    
+    end {
+        Disconnect-ExchangeOnline
+    }
+}
