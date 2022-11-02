ea3536acb6158f6c40c2a6d4c811d4e062dbd242
DevClate <107187714+DevClate@users.noreply.github.com>
2022-11-02 06:11:57 -0400

    Created Function

---
+++ Get-365OutboundGoodMail.ps1
@@ -0,0 +1,31 @@
+function Get-365OutboundGoodMail {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [string]
+        $UserPrincipalName,
+        [Parameter(Mandatory,HelpMessage='Enter start time, ex 04/01/2022')]
+        [datetime]
+        $StartDate,
+        [Parameter(Mandatory,HelpMessage='Enter end date, ex 04/30/2022')]
+        [datetime]
+        $EndDate,
+        [Parameter()]
+        [string]
+        $Path = 'C:\Scripts\OutboundGoodMail.xlsx'
+    )
+    
+    begin {
+        
+    }
+    
+    process {
+         #Outbound Good Mail
+        Get-MailFlowStatusReport -StartDate $startdate -EndDate $enddate -Direction Outbound -EventType GoodMail
+
+    }
+    
+    end {
+        
+    }
+}
