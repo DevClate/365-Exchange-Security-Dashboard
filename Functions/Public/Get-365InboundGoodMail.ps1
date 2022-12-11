0930156ce153fd72d89e78bc6b1c25db8527c97a
DevClate <107187714+DevClate@users.noreply.github.com>
2022-12-11 09:41:21 -0500

    Updated Help

--- Functions/Public/Get-365InboundGoodMail.ps1
+++ Functions/Public/Get-365InboundGoodMail.ps1
@@ -1,4 +1,29 @@
 function Get-365InboundGoodMail {
+            <#
+        .SYNOPSIS
+            PowerShell script to pull all Inbound Good Mail from Exchange Mailflow data into excel
+        .DESCRIPTION
+            PowerShell script to pull all Inbound Good Mail from Exchange Mailflow data into excel with a certain date range.
+            Microsoft doesn't allow past 180 days for Mailflow and only 90 days for Top information. 
+        .PARAMETER
+            -StartDate [datetime]
+            Specifies the starting date that you want to search
+
+            -EndDate [datetime]
+            Specifies the ending date that you want to search
+
+            -Path [string]
+            Specifies the location you want to save the information
+        .NOTES
+            Version:        0.1.0
+            Author:         Clayton Tyger
+            Twitter:        @clatent
+            Github:         DevClate
+           
+        .LINK
+            https://github.com/DevClate/365-Exchange-Security-Dashboard/
+        #>
+
     [CmdletBinding()]
     param (
         [Parameter(Mandatory,HelpMessage='Enter start time, ex 04/01/2022')]
