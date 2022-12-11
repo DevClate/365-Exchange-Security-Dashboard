c0281f42a6933a61224e331477c4dc7016fe43b5
DevClate <107187714+DevClate@users.noreply.github.com>
2022-12-11 09:52:22 -0500

    Updated Help

--- Functions/Public/Get-365TopPhishingRecip.ps1
+++ Functions/Public/Get-365TopPhishingRecip.ps1
@@ -1,4 +1,28 @@
 function Get-365TopPhishingRecip {
+     <#
+        .SYNOPSIS
+            PowerShell script to pull the Top Phishing Recipients from Exchange Mailflow data
+        .DESCRIPTION
+            PowerShell script to pull the Top Phishing Recipients from Exchange Mailflow data with a certain date range.
+            Microsoft doesn't allow past 180 days for Mailflow and only 90 days for Top information. 
+        .PARAMETER
+            -StartDate [datetime]
+            Specifies the starting date that you want to search
+
+            -EndDate [datetime]
+            Specifies the ending date that you want to search
+
+            -Path [string]
+            Specifies the location you want to save the information defaulting to excel
+        .NOTES
+            Version:        0.1.0
+            Author:         Clayton Tyger
+            Twitter:        @clatent
+            Github:         DevClate
+           
+        .LINK
+            https://github.com/DevClate/365-Exchange-Security-Dashboard/
+        #>
     [CmdletBinding()]
     param (
         [Parameter(Mandatory,HelpMessage='Enter start time, ex 04/01/2022')]
