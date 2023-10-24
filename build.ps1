e93e9d3d57b60e1fbce088434655b80fdea8623b
Clayton <107187714+DevClate@users.noreply.github.com>
2023-10-24 05:49:07 -0400

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -1,4 +1,4 @@
 # Grab nuget bits, install modules, set build variables, start build.
 Get-PackageProvider -Name NuGet -ForceBootstrap | Out-Null
 
-Resolve-Module Psake, PSDeploy, Pester, BuildHelpers
+import-Module Psake, PSDeploy, Pester, BuildHelpers
