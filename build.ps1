f452c144d615e43f5b5d0af350a4114e1b62c8da
Clayton <107187714+DevClate@users.noreply.github.com>
2023-10-24 05:51:31 -0400

    Delete build.ps1

--- build.ps1
+++
@@ -1,4 +0,0 @@
-# Grab nuget bits, install modules, set build variables, start build.
-Get-PackageProvider -Name NuGet -ForceBootstrap | Out-Null
-
-import-Module Psake, PSDeploy, Pester, BuildHelpers
